4ed6ac188440cbaca09864e3e716917d5c2f2bff
TimelordUK <sjames8888@gmail.com>
2023-10-01 15:37:17 +0100

    new demo

---
+++ index.js
@@ -0,0 +1,18 @@
+const sql= require('mssql/msnodesqlv8')
+const connectionString  = "Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\node;Database=scratch;Trusted_Connection=yes;"
+
+const config = {
+    connectionString: connectionString
+}
+
+async function runner() {
+    const pool = await sql.connect(config)
+    const res = await pool.query('select top 3 * from syscolumns')
+    console.log(JSON.stringify(res, null, 4))
+    await pool.close()
+}
+
+runner().then(() => {
+    console.log('done')
+})
+
