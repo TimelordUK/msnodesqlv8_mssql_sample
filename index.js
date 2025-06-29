fe1b3f76c5ade997c73a032c7c0eebd63fdebd21
TimelordUK <sjames8888@gmail.com>
2025-06-29 18:37:04 +0100

    update packages.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mssql= require('mssql/msnodesqlv8')
 const {runQuery} = require("./query");
-const connectionString  = "Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\node;Database=scratch;Trusted_Connection=yes;"
+const connectionString = "Driver={ODBC Driver 18 for SQL Server};Server=127.0.0.1,1433;Database=node;UID=node_user;PWD=StrongPassword123!;TrustServerCertificate=yes;Connect Timeout=10";
+
 
 async function runner() {
     const res = await runQuery(mssql, connectionString, 'select top 3 * from syscolumns')
