cdc013685841772851aeef895741ab13073c0b93
TimelordUK <sjames8888@gmail.com>
2025-07-15 20:34:21 +0100

    add stored proc example

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const mssql= require('mssql/msnodesqlv8')
-const {runQuery} = require("./query");
+const {runQuery, executeProcedure} = require("./query");
 const connectionString = "Driver={ODBC Driver 18 for SQL Server};Server=127.0.0.1,1433;Database=node;UID=node_user;PWD=StrongPassword123!;TrustServerCertificate=yes;Connect Timeout=10";
 
 
 async function runner() {
-    const res = await runQuery(mssql, connectionString, 'select top 3 * from syscolumns')
-    console.log(JSON.stringify(res, null, 4))
+    try {
+        const dropProcSQL = `
+            IF EXISTS (SELECT * FROM sys.procedures WHERE name = 'AddNumbers')
+                DROP PROCEDURE AddNumbers
+        `;
+        await runQuery(mssql, connectionString, dropProcSQL);
+        
+        const createProcSQL = `
+            CREATE PROCEDURE AddNumbers
+                @Number1 INT,
+                @Number2 INT,
+                @Result INT OUTPUT
+            AS
+            BEGIN
+                SET @Result = @Number1 + @Number2
+                SELECT @Result AS Sum
+            END
+        `;
+        await runQuery(mssql, connectionString, createProcSQL);
+        console.log('Stored procedure created successfully');
+        
+        const result = await executeProcedure(mssql, connectionString, 'AddNumbers', {
+            Number1: 5,
+            Number2: 7
+        });
+        
+        console.log('Stored procedure result:', JSON.stringify(result, null, 4));
+        
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 runner().then(() => {
--- query.js
+++ query.js
@@ -9,4 +9,24 @@ async function runQuery(mssql, connectionString, sqlString) {
     return res
 }
 
+async function executeProcedure(mssql, connectionString, procedureName, params = {}) {
+    const config = {
+        connectionString: connectionString
+    }
+
+    const pool = await mssql.connect(config)
+    const request = pool.request()
+    
+    for (const [key, value] of Object.entries(params)) {
+        request.input(key, value)
+    }
+    
+    request.output('Result', mssql.Int)
+    
+    const res = await request.execute(procedureName)
+    await pool.close()
+    return res
+}
+
 exports.runQuery = runQuery
+exports.executeProcedure = executeProcedure
